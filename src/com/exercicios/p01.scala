c289eed4ee40533b82af39764f37b919a2cee7e9
aterribili <abner.terribili@gmail.com>
2014-03-06 20:42:32 -0300

    exercício 7 feito

--- src/com/exercicios/p01.scala
+++ src/com/exercicios/p01.scala
@@ -43,17 +43,11 @@ object p01 extends App {
 
   println(flatten(List((1 :: 2 :: 3 :: 6 :: Nil), (1 :: 2 :: 3 :: 5 :: Nil))))
 
-  def flatten(list: List[List[Int]]): List[Int] = {
-    val newList = List[Int]();
-
-    if (list.length == 0) newList;
-
-    newList ::: list(0)
-    list.drop(0)
-
-    newList ::: flatten(list)
-    newList
+  def flatten(list: List[A]): List[A] = ls flatMap {
+    case ms: List[_] => flatten(ms)
+    case e => List(e)
   }
+
 
 }
 
